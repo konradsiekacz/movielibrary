29228185e8b7c7db8643baa2f7f640812853b81e
konradsiekacz <konradsiekacz@gmail.com>
2022-04-18 19:28:48 +0200

    refactored urls

--- src/main/java/com/konrad/movielibrary/MovieController.java
+++ src/main/java/com/konrad/movielibrary/MovieController.java
@@ -2,29 +2,22 @@ package com.konrad.movielibrary;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/movies")
 public class MovieController {
 
     @Autowired
     MovieRepository movieRepository;
 
-    @GetMapping("/hello")
-    public String getHello(){
-        return "hello";
-    }
-
-    @GetMapping("/movies")
+    @GetMapping("/")
     public List<Movie> getAllMovies(){
         return movieRepository.getAllMovies();
     }
-    @GetMapping("/movies/{id}")
+    @GetMapping("/{id}")
     public Movie getMovieById(@PathVariable("id") int id){
        return movieRepository.getMovieById(id);
     }
