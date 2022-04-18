81a4ee5cfbe6dfbd403dd9ef820e107fbbc05b37
konradsiekacz <konradsiekacz@gmail.com>
2022-04-18 12:33:07 +0200

    added method to return all movies

--- src/main/java/com/konrad/movielibrary/MovieController.java
+++ src/main/java/com/konrad/movielibrary/MovieController.java
@@ -1,14 +1,25 @@
 package com.konrad.movielibrary;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
 
+    @Autowired
+    MovieRepository movieRepository;
+
     @GetMapping("/hello")
     public String getHello(){
         return "hello";
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> getAllMovies(){
+        return movieRepository.getAllMovies();
     }
 }
