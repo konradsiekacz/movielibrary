6b706743ad23c7a772315b50f47037e6a6c8761b
konradsiekacz <konradsiekacz@gmail.com>
2022-04-21 17:34:11 +0200

    added entity annotations

--- src/main/java/com/konrad/movielibrary/Movie.java
+++ src/main/java/com/konrad/movielibrary/Movie.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
